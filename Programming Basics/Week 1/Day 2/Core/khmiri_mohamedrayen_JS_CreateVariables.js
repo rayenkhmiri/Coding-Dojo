65b3e1bff93b54018b912cd7a2106a34784f5af9
rayenkhmiri <rayenkhmiri772@icloud.com>
2023-10-19 11:44:04 +0100

    day 2 and day 3 commit

---
+++ Programming Basics/Week 1/Day 2/Core/khmiri_mohamedrayen_JS_CreateVariables.js
@@ -0,0 +1,3 @@
+// variables for our program to determine if a geust can ride  the coaster
+var minimum_age = 10;
+var minimum_height = 42;
---
+++ Programming Basics/Week 1/Day 2/Optional/commenting.js.js
@@ -0,0 +1,9 @@
+// YOUR COMMENT HERE - what would this variable do?
+var likeCount = 0;
+// YOUR COMMENT HERE - what would be the function of this code block?
+function increaseLikes() {
+    // YOUR COMMENT HERE - what is this sort operation for?
+    likeCount = likeCount + 1;
+}
+
+
