bbda4f1bc854cb03ebca3bc01d72694ee341f470
rayenkhmiri <rayenkhmiri772@icloud.com>
2023-10-30 22:06:08 +0100

    commit

---
+++ WebFundamentals/Week 2/Day 1/Practice/Dojo Pizzeria/DojoPizzeria.js
@@ -0,0 +1,63 @@
+function pizzaOven(crustType,sauceType,cheeses,toppings){
+    var pizza ={}
+    pizza.crustType = crustType;
+    pizza.sauceType =sauceType;
+    pizza.cheeses = cheeses;
+    pizza.toppings = toppings;
+    return pizza;
+    
+
+}
+ var pizza 1 = pizzaaOven("deep dish","traditional",["mozarella"],["pepperoni","sausage"]);
+ var pizza 2 = pizzaOven("hand tossed","marinara",["mozarella","feta"],["mushrooms","olives","onions"])
+
+
+var crustType=[
+    "deep dish"
+    "too hot"
+    "spicy"
+    "mozarella"
+    "onions"
+    "hand tossed"
+
+]
+var sauce type=[
+    "deep dish "
+    "marinara"
+    "mozarella"
+    "olives"
+    "onions"
+
+
+]
+var chesses=[
+    "mozzarella",
+    "cheddar",
+    "feta",
+    "vegan cheese"
+    "blue cheese",
+    "swiss cheese",
+    "goat cheese",
+  
+
+]
+
+var toppings=[
+    "pepperoni",
+    "sausage",
+    "chicken",
+    "onions",
+    "olives",
+    "bell peppers",
+   
+    
+    
+]
+
+
+
+
+console.log(randomPizza());
+
+
+
