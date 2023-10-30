a25c4867ee83c3b878c05413e2d4c562e8bf07c2
rayenkhmiri <rayenkhmiri772@icloud.com>
2023-10-30 14:14:30 +0100

    commit

---
+++ WebFundamentals/Week 2/Day 1/Core/ButtonClicker.js
@@ -0,0 +1,14 @@
+console.log("hello from script.js");
+
+
+function removebtn(element){
+    element.innerText = "log out " 
+}    
+function hide(element){
+    element.remove();
+}
+
+function showAlert() {
+    alert("This page says Ninja was liked");
+}
+  
