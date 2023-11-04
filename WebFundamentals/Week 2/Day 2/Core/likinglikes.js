2ee3daaabaa0db38fb368b20a98a748c6e7b44a6
rayenkhmiri <rayenkhmiri772@icloud.com>
2023-11-04 13:38:03 +0100

    commit

--- WebFundamentals/Week 2/Day 2/Core/likinglikes.js
+++ WebFundamentals/Week 2/Day 2/Core/likinglikes.js
@@ -1,6 +1,36 @@
-console.log("hello from script")
+console.log("hello js ");
+var nbr_likes = [0, 0, 0];
 
+function addLike() {
+  console.log("+1");
+  
+  var btn_like = document.querySelector("#nbr_like");
+  nbr_likes[0]++;
+  if (nbr_likes[0] == 1) {
+    btn_like.innerText = nbr_likes[0] + " like";
+  } else {
+    btn_like.innerText = nbr_likes[0] + " likes";
+  }
+}
+function addLike1() {
+  var btn_like = document.querySelector("#nbr_like1");
 
+  nbr_likes[1]++;
 
+  if (nbr_likes[1] == 1) {
+    btn_like.innerText = nbr_likes[1] + " like";
+  } else {
+    btn_like.innerText = nbr_likes[1] + " likes";
+  }
+}
+function addLike2() {
+  var btn_like = document.querySelector("#nbr_like2");
 
+  nbr_likes[2]++;
 
+  if (nbr_likes[2] == 1) {
+    btn_like.innerText = nbr_likes[2] + " like";
+  } else {
+    btn_like.innerText = nbr_likes[2] + " likes";
+  }
+}
