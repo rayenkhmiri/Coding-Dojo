c5de692034f358ff36a11b91c6583397abcc4ef2
rayenkhmiri <rayenkhmiri772@icloud.com>
2023-11-17 22:12:50 +0100

    commit

--- Python/Week 1/Day 4/Practice/understanding_routing/server.py
+++ Python/Week 1/Day 4/Practice/understanding_routing/server.py
@@ -1,27 +1,29 @@
-from flask import Flask  # Import Flask to allow us to create our app
+from flask import Flask
+
+
 app = Flask(__name__)
 
-    # Create a new instance of the Flask class called "app"
-@app.route('/')          # The "@" decorator associates this route with the function immediately following
-def hello_world():
-    return 'Hello World!'
 
+@app.route('/')
+def index():
+    return "Hello World!"
 
 @app.route('/dojo')
 def dojo():
-    return 'dojo!'
+    return "Dojo!"
 
-@app.route('/say/name')
-def name():
-    return 'hi name'
+@app.route('/say/<name>')
+def say_name(name):
+    return f"Hi {name.capitalize()}!"
 
-@app.route('/repeat/<int:counter>/word')
-def say_something(word,counter):
-    return (word + " ")* counter
+@app.route('/repeat/<int:num>/<string:word>')
+def repeat_word(num, word):
+    output = ''
 
+    for i in range(0,num):
+        output += f"<p>{word}</p>"
 
+    return output
 
-
-  # Return the string 'Hello World!' as a response
-if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
-    app.run(debug=True)   
+if __name__=="__main__":
+    app.run(debug=True)
