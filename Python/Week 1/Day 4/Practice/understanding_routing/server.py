4c42cd781637b0409aebadadd42b8cd9f54f4387
rayenkhmiri <rayenkhmiri772@icloud.com>
2023-11-17 22:15:56 +0100

    commit

--- Python/Week 1/Day 4/Practice/understanding_routing/server.py
+++ Python/Week 1/Day 4/Practice/understanding_routing/server.py
@@ -1,6 +1,4 @@
 from flask import Flask
-
-
 app = Flask(__name__)
 
 
@@ -8,22 +6,34 @@ app = Flask(__name__)
 def index():
     return "Hello World!"
 
+
+
 @app.route('/dojo')
 def dojo():
     return "Dojo!"
+
+
+
 
 @app.route('/say/<name>')
 def say_name(name):
     return f"Hi {name.capitalize()}!"
 
+
+
+
 @app.route('/repeat/<int:num>/<string:word>')
 def repeat_word(num, word):
     output = ''
+
 
     for i in range(0,num):
         output += f"<p>{word}</p>"
 
     return output
+
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
