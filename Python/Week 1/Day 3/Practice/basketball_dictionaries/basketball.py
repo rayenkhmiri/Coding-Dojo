36da516d9f27c27d904e86f3a6342a3e1e7f29df
rayenkhmiri <rayenkhmiri772@icloud.com>
2023-11-20 15:32:15 +0100

    commit

---
+++ Python/Week 1/Day 3/Practice/basketball_dictionaries/basketball.py
@@ -0,0 +1,88 @@
+players = [
+    {
+    	"name": "Kevin Durant", 
+    	"age":34, 
+    	"position": "small forward", 
+    	"team": "Brooklyn Nets"
+    },
+    {
+    	"name": "Jason Tatum", 
+    	"age":24, 
+    	"position": "small forward", 
+    	"team": "Boston Celtics"
+    },
+    {
+    	"name": "Kyrie Irving", 
+    	"age":32,
+        "position": "Point Guard", 
+    	"team": "Brooklyn Nets"
+    },
+    {
+    	"name": "Damian Lillard", 
+    	"age":33,
+        "position": "Point Guard", 
+    	"team": "Portland Trailblazers"
+    },
+    {
+    	"name": "Joel Embiid", 
+    	"age":32,
+        "position": "Power Foward", 
+    	"team": "Philidelphia 76ers"
+    },
+    {
+        "name": "DeMar DeRozan",
+        "age": 32,
+        "position": "Shooting Guard",
+        "team": "Chicago Bulls"
+    }
+]
+
+
+class Player:
+    all_players=[]
+    def __init__(self,player_info):
+        self.name = player_info['name']
+        self.age = player_info['age']
+        self.position = player_info['position']
+        self.team = player_info['team']
+        Player.all_players.append(self)
+
+    def display_info(self) :
+        print(f"name is : { self.name}\n age : {self.age}\n position : {self.position}\n team : {self.team}")    
+    
+    @classmethod
+    def get_team (cls,team_list) :
+        new_team_list =[]
+        for i in team_list :Player(i)
+        
+        return new_team_list
+
+
+kevin = {
+    	"name": "Kevin Durant", 
+    	"age":34, 
+    	"position": "small forward", 
+    	"team": "Brooklyn Nets"
+}
+jason = {
+    	"name": "Jason Tatum", 
+    	"age":24, 
+    	"position": "small forward", 
+    	"team": "Boston Celtics"
+}
+kyrie = {
+    	"name": "Kyrie Irving", 
+    	"age":32,
+        "position": "Point Guard", 
+    	"team": "Brooklyn Nets"
+}
+
+player_kevin = Player(kevin)        
+player_jason = Player(jason)    
+player_kyrie = Player(kyrie)    
+
+Player.get_team(players) 
+for i in Player.all_players :
+    i.display_info()
+
+
