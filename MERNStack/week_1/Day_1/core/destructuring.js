080ec1e03ce6ae3f6297ff33402d62266f74a5e7
rayenkhmiri <rayenkhmiri772@icloud.com>
2023-12-18 17:57:53 +0100

    commit

--- MERNStack/week_1/Day_1/core/destructuring.js
+++ MERNStack/week_1/Day_1/core/destructuring.js
@@ -48,18 +48,18 @@ console.log(first == third);
 // false ; true 
 
 // problem 5
-const lastTest = {
-    key: 'value',
-    secondKey: [1, 5, 1, 8, 3, 3]
-}
-const { key } = lastTest;
-const { secondKey } = lastTest;
-const [ ,willThisWork] = secondKey;
-//Predict the output
-console.log(key);
-console.log(secondKey);
-console.log(secondKey[0]);
-console.log(willThisWork);
+// const lastTest = {
+//     key: 'value',
+//     secondKey: [1, 5, 1, 8, 3, 3]
+// }
+// const { key } = lastTest;
+// const { secondKey } = lastTest;
+// const [ ,willThisWork] = secondKey;
+// //Predict the output
+// console.log(key);
+// console.log(secondKey);
+// console.log(secondKey[0]);
+// console.log(willThisWork);
 
 //value[1,5,1,8,3,3]
 //1
