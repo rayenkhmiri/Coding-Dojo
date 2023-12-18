2f053633e44f0c05054f15a57ad32989756c2a48
rayenkhmiri <rayenkhmiri772@icloud.com>
2023-12-18 17:58:10 +0100

    commit

--- MERNStack/week_1/Day_1/core/destructuring.js
+++ MERNStack/week_1/Day_1/core/destructuring.js
@@ -1,37 +1,37 @@
-// const cars = ['Tesla', 'Mercedes', 'Honda']
-// const [ randomCar ] = cars
-// const [ ,otherRandomCar ] = cars
-// //Predict the output
-// console.log(randomCar)
-// console.log(otherRandomCar)
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
 
 // tesla , mercedes
 
-//problem 2
-// const employee = {
-//     name: 'Elon',
-//     age: 47,
-//     company: 'Tesla'
-// }
-// const { name: otherName } = employee;
-// //Predict the output
-// console.log();
-// console.log(otherName);
+// problem 2
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log();
+console.log(otherName);
 
 // error
 
 
 // problem 3
-// const person = {
-//     name: 'Phil Smith',
-//     age: 47,
-//     height: '6 feet'
-// }
-// const password = '12345';
-// const { password: hashedPassword } = person;  
-// //Predict the output
-// console.log(password);
-// console.log(hashedPassword);
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
 
 // 12345 ; undefined
 
@@ -48,18 +48,19 @@ console.log(first == third);
 // false ; true 
 
 // problem 5
-// const lastTest = {
-//     key: 'value',
-//     secondKey: [1, 5, 1, 8, 3, 3]
-// }
-// const { key } = lastTest;
-// const { secondKey } = lastTest;
-// const [ ,willThisWork] = secondKey;
-// //Predict the output
-// console.log(key);
-// console.log(secondKey);
-// console.log(secondKey[0]);
-// console.log(willThisWork);
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
 
 //value[1,5,1,8,3,3]
 //1
