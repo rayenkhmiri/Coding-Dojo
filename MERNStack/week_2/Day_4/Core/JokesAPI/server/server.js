78fe2af97dec8b421de36dc95400c61a667f3e4d
rayenkhmiri <rayenkhmiri772@icloud.com>
2024-01-03 20:18:58 +0100

    commit

--- MERNStack/week_2/Day_4/Core/JokesAPI/server/server.js
+++ MERNStack/week_2/Day_4/Core/JokesAPI/server/server.js
@@ -6,6 +6,7 @@ const port = process.env.port;
 
 // MiddleWare
 // make sure these lines are above any app.get or app.post code blocks
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
