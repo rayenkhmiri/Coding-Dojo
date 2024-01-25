218a24688cbe344a13bd03fd7f4dfb04899985b3
rayenkhmiri <rayenkhmiri772@icloud.com>
2024-01-26 00:17:27 +0100

    commit

--- CSharp/Week1/Day1/Practice/debugging/Practice/Program.cs
+++ CSharp/Week1/Day1/Practice/debugging/Practice/Program.cs
@@ -1,53 +1,7 @@
-﻿// // See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
 
-
-// // Challenge 1
-// bool amProgrammer = "true";
-// int Age = 27.9;
-// List<string> Names = new List<string>();
-// Names = "Monica";
-// Dictionary<string, string> MyDictionary = new Dictionary<string, string>();
-// MyDictionary.Add("Hello", "0");
-// MyDictionary.Add("Hi there", 0);
-// // This is a tricky one! Hint: look up what a char is in C#
-// string MyName = 'MyName';
-// // Challenge 2
-// List<int> Numbers = new List<int>() {2,3,6,7,1,5};
-// for(int i = Numbers.Count; i >= 0; i--)
-// {
-//     Console.WriteLine(Numbers[i]);
-// }
-// // Challenge 3
-// List<int> MoreNumbers = new List<int>() {12,7,10,-3,9};
-// foreach(int i in MoreNumbers)
-// {
-//     Console.WriteLine(MoreNumbers[i]);
-// }
-// // Challenge 4
-// List<int> EvenMoreNumbers = new List<int> {3,6,9,12,14};
-// foreach(int num in EvenMoreNumbers)
-// {
-//     if(num % 3 == 0)
-//     {
-//         num = 0;
-//     }
-// }
-// // Challenge 5
-// // What can we learn from this error message?
-// string MyString = "superduberawesome";
-// MyString[7] = "p";
-// // Challenge 6
-// // Hint: some bugs don't come with error messages
-// Random rand = new Random();
-// int randomNum = rand.Next(12);
-// if(randomNum == 12)
-// {
-//     Console.WriteLine("Hello");
-// }
-
-
-﻿// Challenge 1
+// Challenge 1
 bool amProgrammer = true;
 double Age = 27.9;
 List<string> Names = new List<string>();
@@ -55,7 +9,7 @@ Names.Add("Monica");
 Dictionary<string, string> MyDictionary = new Dictionary<string, string>();
 MyDictionary.Add("Hello", "0");
 MyDictionary.Add("Hi there", "0");
-// This is a tricky one! Hint: look up what a char is in C#
+// This is a tricky one : look up what a char 
 string MyName = "MyName";
 // Challenge 2
 List<int> Numbers = new List<int>() {2, 3, 6, 7, 1, 5};
@@ -65,10 +19,10 @@ for (int i = Numbers.Count - 1; i >= 0; i--)
 }
 
 // Challenge 3
-List<int> MoreNumbers = new List<int>() {12,7,10,-3,9};
-foreach(int i in MoreNumbers)
+List<int> MoreNumbers = new List<int>() {12, 7, 10, -3, 9};
+foreach (int num in MoreNumbers)
 {
-    Console.WriteLine(i);
+    Console.WriteLine(num);
 }
 // Challenge 4
 List<int> EvenMoreNumbers = new List<int> {3, 6, 9, 12, 14};
@@ -81,7 +35,7 @@ for (int i = 0; i < EvenMoreNumbers.Count; i++)
     }
 }
 
-// Now EvenMoreNumbers will be modified, and you can print or use the modified list.
+
 foreach (int num in EvenMoreNumbers)
 {
     Console.WriteLine(num);
@@ -90,14 +44,17 @@ foreach (int num in EvenMoreNumbers)
 // Challenge 5
 // What can we learn from this error message?
 string MyString = "superduberawesome";
-MyString[7] = 'p';
+// Create a new string .
+Console.WriteLine(MyString);
+
 // Challenge 6
-// Hint: some bugs don't come with error messages
+
 Random rand = new Random();
-int randomNum = rand.Next(12,12);
-if(randomNum == 12)
+int randomNum = rand.Next(12);
+if (randomNum == 12)
 {
     Console.WriteLine("Hello");
 }
+
 
 
