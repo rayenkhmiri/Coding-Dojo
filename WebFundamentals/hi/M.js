c69faeca34c2802cf704a8b59bc7c4d6ba5b49fd
rayenkhmiri <rayenkhmiri772@icloud.com>
2024-02-07 22:27:12 +0100

    commit

---
+++ WebFundamentals/hi/M.js
@@ -0,0 +1,20 @@
+function showAlert() {
+  alert(
+    "HyperText Markup Language, commonly abbreviated HTML or, in its latest version, HTML5, is the markup language designed to represent web pages."
+  );
+}
+function showAlert() {
+  alert(
+    "Cascading Style Sheets, generally called CSS, form a computer language that describes the presentation of HTML and XML documents. The standards defining CSS are published by the World Wide Web Consortium."
+  );
+}
+function showAlert() {
+  alert(
+    "JavaScript is a scripting programming language primarily used in interactive web pages and as such is an essential part of web applications. Along with HTML and CSS, JavaScript is at the heart of the languages ​​used by web developers"
+  );
+}
+function showAlert() {
+  alert(
+    "C# is an object-oriented programming language, marketed by Microsoft since 2002 and intended to develop on the Microsoft .NET platform, in the same way as other languages ​​linked to this"
+  );
+}
