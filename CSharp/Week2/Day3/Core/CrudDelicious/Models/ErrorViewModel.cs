ab665dc686857c727647940abe7760cda03dd4b8
rayenkhmiri <rayenkhmiri772@icloud.com>
2024-02-07 11:36:19 +0100

    commit

--- CSharp/Week2/Day3/Core/CrudDelicious/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace CrudDelicious.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
